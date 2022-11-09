3318d5c3214fe5096eb45a3fc19eb743ffa1f89e
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-11-09 11:05:57 +0300

    'bot'

---
+++ bot.py
@@ -0,0 +1,5 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+bot = Bot(token='5380146858:AAHLjQQaCe2JCdD40mP-CwGGnIyYABoKa4I')
+dp = Dispatcher(bot)
---
+++ handlers.py
@@ -0,0 +1,13 @@
+from aiogram import Dispatcher
+
+import commands
+
+def registred_handlers(dp: Dispatcher):
+    dp.register_message_handler(commands.start, commands=['start'])
+    dp.register_message_handler(commands.finish, commands=['finish'])
+    dp.register_message_handler(commands.help, commands=['help'])
+    dp.register_message_handler(commands.rules, commands=['rules'])
+    dp.register_message_handler(commands.game, commands=['game'])
+    dp.register_message_handler(commands.getNumber)
+
+    # dp.register_message_handler(commands.error) # Эту команду ставим строго в конце остальных команд, иначе другие команды не сработают
